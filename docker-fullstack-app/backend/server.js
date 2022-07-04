92ff2c2229a8ee45046562fc75cf489450fea489
Chan <linsay070@gmail.com>
2022-07-04 18:58:24 +0900

    fixed.

--- docker-fullstack-app/backend/server.js
+++ docker-fullstack-app/backend/server.js
@@ -11,18 +11,18 @@ const app = express();
 app.use(bodyParser.json());
 
 // 테이블 생성하기
-db.pool.query('CREATE TABLE lists' (
+db.pool.query(`CREATE TABLE lists (
     id INTEGER AUTO_INCREMENT,
     value Text,
     PRIMARY KEY (id)
-), (err, results, fields) => {
+)`, (err, results, fields) => {
     console.log('results', results)
 })
 
 // DB lists 테이블에 있는 모든 데이터를 프론트 서버에 보내주기
 app.get('/api/values', function( req, res ) {
     // DB에서 모든 정보 가져오기
-    db.pool.query('SELECT * FROM lists;',
+    db.pool.query(`SELECT * FROM lists;`,
         (err, results, fields) => {
         if(err)
             return res.status(500).send(err)
@@ -34,7 +34,7 @@ app.get('/api/values', function( req, res ) {
 // 클라이언트에서 입력한 값을 데이터베이스 lists 테이블에 넣어주기
 app.post('/api/value', function(req, res, next) {
     // DB에 값 넣어주기
-    db.pool.query('INSERT INTO lists (value) VALUES("${req.body.value}")',
+    db.pool.query(`INSERT INTO lists (value) VALUES("${req.body.value}")`,
         (err, results, fields) => {
             if(err)
                 return res.status(500).send(err)
