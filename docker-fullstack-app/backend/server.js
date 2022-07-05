38f290dd2dcd3e072e1cd7a95d1544354293c58f
Chan <linsay070@gmail.com>
2022-07-05 13:43:17 +0900

    .

--- docker-fullstack-app/backend/server.js
+++ docker-fullstack-app/backend/server.js
@@ -11,13 +11,13 @@ const app = express();
 app.use(bodyParser.json());
 
 // 테이블 생성하기
-db.pool.query(`CREATE TABLE lists (
-    id INTEGER AUTO_INCREMENT,
-    value Text,
-    PRIMARY KEY (id)
-)`, (err, results, fields) => {
-    console.log('results', results)
-})
+// db.pool.query(`CREATE TABLE lists (
+//     id INTEGER AUTO_INCREMENT,
+//     value Text,
+//     PRIMARY KEY (id)
+// )`, (err, results, fields) => {
+//     console.log('results', results)
+// })
 
 // DB lists 테이블에 있는 모든 데이터를 프론트 서버에 보내주기
 app.get('/api/values', function( req, res ) {
